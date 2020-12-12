8c9ef6cc87f7bf2cf0e409b7d9afde3959d8eada
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-12 17:23:47 -0500

    minor style and dec counter tested

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React, { useState } from 'react';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [error, setError] = React.useState(false);
+
   return (
-    <div data-test="component-app">
+    <div data-test="component-app" className="App">
       <h1 data-test="counter-display">
         The counter is currently&nbsp;
         <span data-test="count">{count}</span>
       </h1>
-      <button 
+      {/* Notes: 
+      - using ternary on the error state to determine whether or not to hide 
+      - the 'error' and 'hidden' classes are defined in App.css
+      */}
+      <div data-test="error-message" className={`error ${error ? '' : 'hidden'}`}>
+        The counter cannot go below 0
+      </div>
+      <button
         data-test="increment-button"
-        onClick={() => setCount(count + 1)}
-      >Increment Counter</button>
+        onClick={() => {
+          if (error) { setError(false); }
+          setCount(count + 1)
+        }
+      }
+      >
+        Increment counter
+      </button>
+      <button
+        data-test="decrement-button"
+        onClick={() => {
+          if (count > 0) {
+            setCount(count - 1)
+          } else {
+            setError(true);
+          }
+        }
+      }
+      >
+        Decrement counter
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -44,10 +44,86 @@ test("clicking on button increments counter display", () => {
 
   //find the button
   const button = findByTestAttr(wrapper, 'increment-button');
-  button.simulate('click');
+
   //click the button
+  button.simulate('click');
+  
 
   //find the display, and test the number has been incremented
   const count = findByTestAttr(wrapper, 'count').text();
   expect(count).toBe("1");
 });
+
+describe('decrement button', () => {
+  test('renders decrement button', () => {
+    const wrapper = setup();
+    const button = findByTestAttr(wrapper, 'decrement-button');
+    expect(button.length).toBe(1);
+  });
+
+  test('clicking decrement button decrements counter display when state is greater than 0', () => {
+    const wrapper = setup();
+
+    // click the increment button so that the counter is greater than 0
+    const incButton = findByTestAttr(wrapper, 'increment-button');
+    incButton.simulate('click');
+
+    // find decrement button and click
+    const decButton = findByTestAttr(wrapper, 'decrement-button');
+    decButton.simulate('click');
+
+    // find display and test value
+    const count = findByTestAttr(wrapper, 'count').text();
+    expect(count).toBe("0");
+  });
+});
+
+describe('error when counter goes below 0', () => {
+  test('error does not show when not needed', () => {
+    // I plan to implement this by using a "hidden" class for the error div
+    // I plan to use the data-test value 'error-message' for the error div
+    const wrapper = setup();
+    const errorDiv = findByTestAttr(wrapper, 'error-message');
+
+    // using enzyme's ".hasClass()" method
+    // http://airbnb.io/enzyme/docs/api/ShallowWrapper/hasClass.html
+    const errorHasHiddenClass = errorDiv.hasClass('hidden');
+    console.log(wrapper.debug());
+    expect(errorHasHiddenClass).toBe(true);
+  });
+  describe('counter is 0 and decrement is clicked', () => {
+    // using a describe here so I can use a "beforeEach" for shared setup
+
+    // scoping wrapper to the describe, so it can be used in beforeEach and the tests
+    let wrapper
+    beforeEach(() => {
+      // no need to set counter value here; default value of 0 is good
+      wrapper = setup();
+
+      // find button and click
+      const button = findByTestAttr(wrapper, 'decrement-button');
+      button.simulate('click');
+    });
+    test('error shows', () => {
+      // check the class of the error message
+      const errorDiv = findByTestAttr(wrapper, 'error-message');
+      const errorHasHiddenClass = errorDiv.hasClass('hidden');
+      expect(errorHasHiddenClass).toBe(false);
+    });
+    test('counter still displays 0', () => {
+      const count = findByTestAttr(wrapper, 'count').text();
+      expect(count).toBe("0");
+    });
+    test('clicking increment clears the error', () => {
+      // find and click the increment button
+      const incButton = findByTestAttr(wrapper, 'increment-button');
+      incButton.simulate('click');
+
+      // check the class of the error message
+      const errorDiv = findByTestAttr(wrapper, 'error-message');
+      const errorHasHiddenClass = errorDiv.hasClass('hidden');
+      expect(errorHasHiddenClass).toBe(true);
+    });
+  });
+});
+
